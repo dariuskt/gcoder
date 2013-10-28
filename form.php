16a164635a746859780655b204b9a29b3376b589
darius <darius@vozbutas.lt>
2013-10-28 09:43:57 +0200

    few html tags fixed

--- form.php
+++ form.php
@@ -6,35 +6,35 @@
 	</head>
 	<body>
 
-
+<hr>
 <form action="?" method="GET">
 	<label for="outsideDiameter">outsideDiameter</label>
 	<input name="outsideDiameter" id="outsideDiameter" type="text" value="<?=@$_GET['outsideDiameter']?>">
-<br/>
+<br>
 	<label for="pitchDiameter">pitchDiameter</label>
 	<input name="pitchDiameter" id="pitchDiameter" type="text" value="<?=@$_GET['pitchDiameter']?>">
-<br/>
+<br>
 	<label for="toothCount">toothCount</label>
 	<input name="toothCount" id="toothCount" type="text" value="<?=@$_GET['toothCount']?>">
-<br/>
+<br>
 	<label for="toothDepth">toothDepth</label>
 	<input name="toothDepth" id="toothDepth" type="text" value="<?=@$_GET['toothDepth']?>">
-<br/>
+<br>
 	<label for="gearWidth">gearWidth</label>
 	<input name="gearWidth" id="gearWidth" type="text" value="<?=@$_GET['gearWidth']?>">
-<br/>
+<br>
 	<label for="cutterDiameter">cutterDiameter</label>
 	<input name="cutterDiameter" id="cutterDiameter" type="text" value="<?=@$_GET['cutterDiameter']?>">
-<br/>
+<br>
 	<label for="gearType">gearType</label>
 	<select name="gearType" id="gearType">
 		<option value="rh" <?=('rh'==@$_GET['gearType'])?'selected="selected"':'';?>>Right Hand</option>
 		<option value="lh" <?=('lh'==@$_GET['gearType'])?'selected="selected"':'';?>>Left Hand</option>
 	</select>
-<br/>
+<br>
 	<label for="angle">angle</label>
 	<input name="angle" id="angle" type="text" value="<?=@$_GET['angle']?>">
-<hr/>
+<hr>
 	<label for="cutFrom">cutFrom</label>
 	<select name="cutFrom" id="cutFrom">
 		<option value="-1" <?=(@$_GET['cutFrom']<0)?'selected="selected"':'';?>>Y negative (front)</option>
@@ -43,19 +43,19 @@
 <br>
 	<label for="roughingStepDown">roughingStepDown</label>
 	<input name="roughingStepDown" id="roughingStepDown" type="text" value="<?=@$_GET['roughingStepDown']?>">
-<br/>
+<br>
 	<label for="finishingStepDown">finishingStepDown</label>
 	<input name="finishingStepDown" id="finishingStepDown" type="text" value="<?=@$_GET['finishingStepDown']?>">
-<br/>
+<br>
 	<label for="safetyDistance">safetyDistance</label>
 	<input name="safetyDistance" id="safetyDistance" type="text" value="<?=@$_GET['safetyDistance']?>">
-<br/>
+<br>
 	<label for="feed">feed</label>
 	<input name="feed" id="feed" type="text" value="<?=@$_GET['feed']?>">
-<br/>
+<br>
 	<label for="seek">seek</label>
 	<input name="seek" id="seek" type="text" value="<?=@$_GET['seek']?>">
-<br/>
+<br>
 	<input name="generate" id="generate" type="submit" value="generate">
 	<input name="download" id="download" type="submit" value="download">
 
