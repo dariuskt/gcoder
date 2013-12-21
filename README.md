89186ed62a02acad53bd32efc1b4db38d94bc603
darius <darius@vozbutas.lt>
2013-12-21 14:41:59 +0200

    Adding link to github and more info to readme.

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ gcoder
 ======
 
 gcoder is a g-code generator for simple-but-complex stuff. 
-At a time gcoder generates g-code for cutting helical gears.
+At a time gcoder generates g-code for cutting helical and spur gears
+on a 4 axis CNC mill.
 
--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php
-$version = 'v0.2.0+';
+$version = 'v0.2.1';
 ?>
 <html>
 	<head>
@@ -19,7 +19,7 @@ $version = 'v0.2.0+';
 </aside>
 <aside>
 	Want to help? Contact me: 
-	<mark><script>document.write('gcoder+<?=$version?>ATatDOTlt'.replace('AT','@').replace('DOT','.'))</script></mark>
+	<mark><script>document.write('gcoderATatDOTlt'.replace('AT','@').replace('DOT','.'))</script></mark>
 </aside>
 <hr>
 <form action="?" method="GET">
@@ -90,8 +90,15 @@ $version = 'v0.2.0+';
 	</form>
  
 	and GitTip: <script data-gittip-username="dariuskt"
-        data-gittip-widget="button"
-        src="//gttp.co/v1.js"></script>
+		data-gittip-widget="button"
+		src="//gttp.co/v1.js"></script>
+
+	Source can be found on <a href="https://github.com/dariuskt/gcoder">GitHub</a>
+
+	<a href="https://github.com/dariuskt/gcoder"><img 
+		style="position: absolute; top: 0; right: 0; border: 0;" 
+		src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" 
+		alt="Fork me on GitHub"></a>
 
 </aside>
 
